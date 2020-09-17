4bf299a4fbf4d99f9b07baf90c95223abe9eca0b
Alexis Lyndon Galaura <alexislyndon@gmail.com>
2020-09-17 10:25:35 +0800

    3rd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.use(express.json());
 const port = 9999;
 
 app.get("/", (req, res) => {
-    res.sendfile('index.html');
+  res.sendfile("index.html");
 });
 
 // app.post("/msg", (req, res) => {
@@ -13,8 +13,12 @@ app.get("/", (req, res) => {
 // });
 
 app.post("/", (req, res) => {
-    var msg = req.body.msg
-    res.send(`<form action="/" method="post"><label for="msg">Message: </label><input type="text" name="msg" id=""><input type="submit" value="Send"><br><br>${"Message: " + msg + "<br> length:  "+ msg.length}</form>`);
+  var msg = req.body.msg;
+  res.send(
+    `<form action="/" method="post"><label for="msg">Message: </label><input type="text" name="msg" id=""><input type="submit" value="Send"><br><br>${
+      "Message: " + msg + "<br> length:  " + msg.length
+    }</form>`
+  );
 });
 
 app.listen(port, () => {
